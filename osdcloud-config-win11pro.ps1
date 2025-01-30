4a3e71c2d9a40d63050bbf35ec8c7e9b1d7ece8a
Mike Hill <mike.hill@techahoy.com>
2025-01-30 20:27:26 +0000

    Update osdcloud-config-win11pro.ps1

--- osdcloud-config-win11pro.ps1
+++ osdcloud-config-win11pro.ps1
@@ -3,9 +3,11 @@ Start-Sleep -Seconds 5
 
 Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Pro -OSLanguage en-gb -OSLicense Retail -Firmware
 
+Write-Host -ForegroundColor Green "Installing Windows 11 Pro 24H2 en-gb"
+
 #Restart from WinPE
 
-Write-Host -ForegroundColor Green “Restarting in 20 seconds!”
+Write-Host -ForegroundColor Green "Restarting in 20 seconds!"
 
 Start-Sleep -Seconds 20
 
