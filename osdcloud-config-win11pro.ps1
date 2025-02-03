2820c3d1c3b0ac8ae4e8bb022d7047ad1240b815
Mike Hill <mike.hill@techahoy.com>
2025-02-03 11:32:44 +0000

    Update osdcloud-config-win11pro.ps1

--- osdcloud-config-win11pro.ps1
+++ osdcloud-config-win11pro.ps1
@@ -1,6 +1,10 @@
 Write-Host -ForegroundColor Green "Starting OSDCloud"
 Start-Sleep -Seconds 5
 
+Write-Host "Updating Surface Driver Catalog"
+Invoke-RestMethod "https://raw.githubusercontent.com/everydayintech/OSDCloud-Public/refs/heads/main/Catalogs/Update-OSDCloudSurfaceDriverCatalogJustInTime.ps1"
+Update-OSDCloudSurfaceDriverCatalogJustInTime -UpdateDriverPacksJson
+
 Start-OSDCloud -OSVersion 'Windows 11' -OSBuild 24H2 -OSEdition Pro -OSLanguage en-gb -OSLicense Retail -Firmware
 
 Write-Host -ForegroundColor Green "Installing Windows 11 Pro 24H2 en-gb"
