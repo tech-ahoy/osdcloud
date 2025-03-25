6ebff24d4a05e01b9e9f31e1f82ece01e3e87f4d
Mike Hill <mike.hill@techahoy.com>
2025-03-25 12:10:54 +0000

    Added Win 11 Pro 23H2

--- osdcloud-menu.ps1
+++ osdcloud-menu.ps1
@@ -6,8 +6,9 @@ do {
     Write-Host "====================================="
     Write-Host "1. Windows 11 Pro 24H2 OOBE"
     Write-Host "2. Windows 11 Pro 24H2 Autounattend"
-    Write-Host "3. Windows 10 Pro 22H2 OOBE"
-    Write-Host "4. Exit"
+    Write-Host "3. Windows 11 Pro 23H2 Autounattend"
+    Write-Host "4. Windows 10 Pro 22H2 OOBE"
+    Write-Host "5. Exit"
     Write-Host "====================================="
     $choice = Read-Host "Please select an option (1-4)"
     
@@ -22,10 +23,14 @@ do {
             Invoke-RestMethod "https://raw.githubusercontent.com/tech-ahoy/osdcloud/refs/heads/main/osdcloud-config-win11pro-autounattend.ps1" | Invoke-Expression
         }
         3 {
+            Write-Host "`nSelected: Windows 11 Pro 23H2 OOBE" -ForegroundColor Cyan
+            Invoke-RestMethod "https://raw.githubusercontent.com/tech-ahoy/osdcloud/refs/heads/main/osdcloud-config-win11pro-23h2-oobe.ps1" | Invoke-Expression
+        }
+        4 {
             Write-Host "`nSelected: Windows 10 Pro 22H2 OOBE" -ForegroundColor Cyan
             Invoke-RestMethod "https://raw.githubusercontent.com/tech-ahoy/osdcloud/refs/heads/main/osdcloud-config-win10pro-oobe.ps1" | Invoke-Expression
         }
-        4 {
+        5 {
             Write-Host "Exiting the script. Goodbye!" -ForegroundColor Green
             break
         }
