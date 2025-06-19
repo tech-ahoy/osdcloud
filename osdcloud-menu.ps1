82d20d629800fc895781d0233f6150a6580472d0
Mike Hill <mike.hill@techahoy.com>
2025-06-19 09:58:08 +0100

    Added Win 11 Home 24H2 OOBE option

--- osdcloud-menu.ps1
+++ osdcloud-menu.ps1
@@ -4,24 +4,26 @@ do {
     Write-Host "====================================="
     Write-Host "         Tech Ahoy OSDCloud          "
     Write-Host "====================================="
-    Write-Host "1. Windows 11 Pro 24H2 OOBE"
+    Write-Host "1. Windows 11 Pro 24H2 OOBE (NinjaOne)"
     Write-Host 
     Write-Host "2. Windows 11 Pro 24H2 Autopilot"
     Write-Host
     Write-Host "3. Windows 11 Pro 24H2 Autounattend"
     Write-Host
-    Write-Host "4. Windows 11 Pro 23H2 OOBE"
+    Write-Host "4. Windows 11 Home 24H2 OOBE (NinjaOne)"
     Write-Host
-    Write-Host "5. Windows 10 Pro 22H2 OOBE"
+    Write-Host "5. Windows 11 Pro 23H2 OOBE (NinjaOne)"
     Write-Host
-    Write-Host "6. Exit"
+    Write-Host "6. Windows 10 Pro 22H2 OOBE (NinjaOne)"
+    Write-Host
+    Write-Host "7. Exit"
     Write-Host "====================================="
     Write-Host
     $choice = Read-Host "Please select an option (1-4)"
     
     switch ($choice) {
         1 {
-            Write-Host "`nSelected: Windows 11 Pro 24H2 OOBE" -ForegroundColor Cyan
+            Write-Host "`nSelected: Windows 11 Pro 24H2 OOBE (NinjaOne)" -ForegroundColor Cyan
             Invoke-RestMethod "https://raw.githubusercontent.com/tech-ahoy/osdcloud/refs/heads/main/osdcloud-config-win11pro-24h2-oobe.ps1" | Invoke-Expression
             
         }
@@ -34,14 +36,18 @@ do {
             Invoke-RestMethod "https://raw.githubusercontent.com/tech-ahoy/osdcloud/refs/heads/main/osdcloud-config-win11pro-24h2-autounattend.ps1" | Invoke-Expression
         }
         4 {
-            Write-Host "`nSelected: Windows 11 Pro 23H2 OOBE" -ForegroundColor Cyan
-            Invoke-RestMethod "https://raw.githubusercontent.com/tech-ahoy/osdcloud/refs/heads/main/osdcloud-config-win11pro-23h2-oobe.ps1" | Invoke-Expression
+            Write-Host "`nSelected: Windows 11 Home 24H2 OOBE (NinjaOne)" -ForegroundColor Cyan
+            Invoke-RestMethod "https://raw.githubusercontent.com/tech-ahoy/osdcloud/refs/heads/main/osdcloud-config-win11home-24h2-oobe.ps1" | Invoke-Expression
         }
         5 {
-            Write-Host "`nSelected: Windows 10 Pro 22H2 OOBE" -ForegroundColor Cyan
-            Invoke-RestMethod "https://raw.githubusercontent.com/tech-ahoy/osdcloud/refs/heads/main/osdcloud-config-win10pro-22h2-oobe.ps1" | Invoke-Expression
+            Write-Host "`nSelected: Windows 11 Pro 23H2 OOBE (NinjaOne)" -ForegroundColor Cyan
+            Invoke-RestMethod "https://raw.githubusercontent.com/tech-ahoy/osdcloud/refs/heads/main/osdcloud-config-win11pro-23h2-oobe.ps1" | Invoke-Expression
         }
         6 {
+            Write-Host "`nSelected: Windows 10 Pro 22H2 OOBE (NinjaOne)" -ForegroundColor Cyan
+            Invoke-RestMethod "https://raw.githubusercontent.com/tech-ahoy/osdcloud/refs/heads/main/osdcloud-config-win10pro-22h2-oobe.ps1" | Invoke-Expression
+        }
+        7 {
             Write-Host "Exiting the script. Goodbye!" -ForegroundColor Green
             break
         }
